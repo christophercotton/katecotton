3f2ab16425701377f33aeb84811132ab4ef73f83
Christopher Cotton <me@christophercotton.com>
2021-02-24 08:57:15 -0600

    First commit

---
+++ Kate_Cotton/Blog/Blog_files/Blog.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100113
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("http://katecotton.com/Kate_Cotton/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Kate_Cotton/Blog/Entries/2010/1/13_Entry_1-_the_comment_tester_files/13_Entry_1-_the_comment_tester.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100113
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('13_Entry_1-_the_comment_tester_files/13_Entry_1-_the_comment_testerMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
